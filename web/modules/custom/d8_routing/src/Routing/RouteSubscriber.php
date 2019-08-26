90d45230fb4b272d8860b810cf6681189790ee53
msuthars <msuthar1994@gmail.com>
2019-08-26 17:12:34 +0530

    Drupal 8 Routing Activity

---
+++ web/modules/custom/d8_routing/src/Routing/RouteSubscriber.php
@@ -0,0 +1,22 @@
+<?php 
+
+namespace Drupal\d8_routing\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Listens to the dynamic route events.
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+    if ($route = $collection->get('system.site_information_settings')) {
+      $route->setDefault('_form', 'Drupal\d8_routing\Form\ApiSiteInformationForm');
+    }
+  }
+
+}
